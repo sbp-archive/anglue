a6ef769221d50330ef914fa74e4490308c621bb1
Tommy Maintz <tommy.maintz@gmail.com>
2015-10-20 16:09:02 +0200

    release 1.0.10

--- src/application.js
+++ src/application.js
@@ -63,6 +63,13 @@ export class ApplicationAnnotation extends Annotation {
             Reflect.deleteProperty(routes, 'defaultRoute');
           }
 
+          if (routes.redirects) {
+            Object.keys(routes.redirects).forEach(fromRoute => {
+              $urlRouterProvider.when(fromRoute, routes.redirects[fromRoute]);
+            });
+            Reflect.deleteProperty(routes, 'redirects');
+          }
+
           Object.keys(routes).forEach(name => {
             $stateProvider.state(name, routes[name]);
           });
